fe2ebf5ef7491c520abb8c49ce9705ba91d2baf8
muratozbk <slipstream11@yandex.com>
2023-10-08 17:49:49 +0300

    responsive nav items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,35 @@ import { Link } from 'react-router-dom'
 import { MenuItems } from './MenuItems'
 
 class Navbar extends Component {
+    state = { clicked: false }
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
     render() {
         return (
             <>
                 <nav className='NavbarItems'>
                     <h1 className="navbar-logo">Trek Tripper</h1>
 
+                    <div className="menu-icons"
+                        onClick={this.handleClick}>
+                        <i className={`fas ${this.state.clicked ?
+                            "fa-times" : "fa-bars"}`}></i>
+                    </div>
+
                     <ul className='nav-menu'>
                         {MenuItems.map((item, ind) => (
                             <li key={ind} className={item.cName}>
                                 <Link to={item.url} >
-                                    <i className={item.icon}> </i>
-                                    <span className='itemTitle'>   {item.title}</span>
+                                    <span className='itemTitle'>
+                                        <i className={item.icon}> </i>
+                                        {item.title}</span>
                                 </Link>
                             </li>
                         ))}
 
+                        <button>Sign Up</button>
                     </ul>
                 </nav>
             </>
