7791653f3007f77a221af054dca88726f7c903a5
muratozbk <slipstream11@yandex.com>
2023-10-10 21:18:05 +0300

    dynamic destination

---
+++ src/components/DestinationDatas.js
@@ -0,0 +1,29 @@
+import Mountain1 from '../assets/1.jpg'
+import Mountain2 from '../assets/2.jpg'
+import Mountain3 from '../assets/3.jpg'
+import Mountain4 from '../assets/4.jpg'
+import './DestinationStyles.css';
+
+import React from 'react'
+
+function DestinationDatas(props) {
+    return (
+        <div className={props.className}>
+            <div className="des-text">
+                <h2>{props.heading} </h2>
+                <p>
+                    {props.text}
+                </p>
+            </div>
+
+            <div className="image">
+                <img src={props.img1} alt="destination1" />
+                <img src={props.img2} alt="destination2" />
+            </div>
+
+        </div>
+    )
+}
+
+export default DestinationDatas
+// 2.30.35
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './NavbarStyles.css'
 import { Link } from 'react-router-dom'
-import { MenuItems } from './MenuItems'
+import { MenuItems } from '../data/MenuItems'
 
 class Navbar extends Component {
     state = { clicked: false }
